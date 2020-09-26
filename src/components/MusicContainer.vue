0ffa2e679896027abfd09f323cd08fa6ec19f889
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-26 15:36:50 -0500

    add API rest
    logre traer los datos de la api de deezer

---
+++ src/components/MusicContainer.vue
@@ -0,0 +1,33 @@
+<template>
+      <div>
+      {{ deezerData }}
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "musicContainer",
+  mounted() {
+    axios
+      //http://api.deezer.com/playlist/436962 utilice un dominio diferente para no tener problemas con el CORS de api deezer. https://developers.deezer.com/api
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/436962",
+        {
+          method: "GET",
+          crossDomain: true
+        }
+      )
+      .then(res => {
+        this.deezerData = res.data;
+        console.log(res);
+      });
+  },
+  data(){
+      return{
+          deezerData :'',
+      }
+  }
+};
+</script>
+<style lang="scss">
+</style>
