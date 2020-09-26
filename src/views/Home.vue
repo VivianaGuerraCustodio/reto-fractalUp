4d1d480131f0d7952bb13e36e7bf623622ae2ddc
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-26 15:39:57 -0500

    add component of musicContainer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,17 @@
   <div>
     <navigationColumn />
     <search />
-    <div>
-      {{deezerData}}
-    </div>
+<musicContainer/>
     <musicPlayer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios';
+
 import navigationColumn from "@/components/NavigationColumn.vue";
 import Search from "@/components/SearchText.vue";
+import musicContainer from "@/components/MusicContainer.vue"
 import musicPlayer from "@/components/MusicPlayer.vue";
 
 export default {
@@ -21,25 +20,16 @@ export default {
   components: {
     navigationColumn,
     musicPlayer,
-    Search
+    Search,
+    musicContainer
   },
-  data(){
-    return{
-      deezerData:'',
-    }
+  data() {
+    return {
+      deezerData: ""
+    };
   },
-  created(){
-    axios.get('https://api.deezer.com/playlist/436962',{
-     "method":"GET",
-     "crossDomain":true,
-    })
-    .then((res)=>{
-      this.deezerData = res;
-      console.log(res);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+  mounted() {
+
   }
 };
 </script>
