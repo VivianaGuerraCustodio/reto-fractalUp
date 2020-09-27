22335101feb369cd4f230fde57dee96d59bbd076
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-27 03:23:51 -0500

    add icon -menu-hamburguer

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faPlayCircle, faStepForward, faStepBackward } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faPlayCircle, faStepForward, faStepBackward,faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
@@ -10,6 +10,7 @@ library.add(
   faPlayCircle,
   faStepForward,
   faStepBackward,
+  faEllipsisV,
 )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
