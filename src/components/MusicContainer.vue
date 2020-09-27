79381d88dc2c4bcefb650c77c53733a48afed976
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-27 02:46:00 -0500

    edit data of json in the interface

--- src/components/MusicContainer.vue
+++ src/components/MusicContainer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div class="info-music" v-for="(info, index) in deezerData" :key="index">
-      <p>{{ info.artist.name }}</p>
-      <p>{{ info.title }}</p>
-      <img :src="info.album.cover" >
+  <div class="player-music">
+    <div class="data-music" v-for="(info, index) in deezerData" :key="index">
+      <img class="img-artist" :src="info.album.cover" />
+      <p class="title-music">{{ info.title }}</p>
+      <p class="artist-name">{{ info.artist.name }}</p>
     </div>
   </div>
 </template>
@@ -24,20 +24,56 @@ export default {
   },
   data() {
     return {
-      deezerData: "",
+      deezerData: ""
     };
   }
 };
 </script>
 <style lang="scss">
-.container-info-music {
+.player-music {
   box-sizing: border-box;
   padding-bottom: 82px;
-  margin-bottom: 0px;
-  margin: auto auto auto 89px;
-  .info-music {
+  margin-left: 250px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  .data-music {
+    display: flex;
+    flex-direction: column;
+    padding: 0 14px;
+    margin: 11px;
+    width: 188px;
     background-color: green;
     box-sizing: border-box;
+    .title-music {
+      //styleName: 14-bold-qs;
+      color: #555555;
+      font-family: Quicksand;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 18px;
+      letter-spacing: 0em;
+      text-align: left;
+      margin-top:7px;
+      margin-bottom:1px;
+    }
+    .artist-name {
+      //styleName: 12-regular-qs;
+      color: #828282;
+      font-family: Quicksand;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 15px;
+      letter-spacing: 0em;
+      text-align: left;
+      margin-top:1px;
+    }
+    .img-artist {
+      width: 160px;
+      height: 160px;
+    }
   }
 }
 </style>
