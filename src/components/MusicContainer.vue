a811049042004c709fc9a4d4a340c921a4a9843e
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2020-09-26 22:15:46 -0500

    edited music container information

--- src/components/MusicContainer.vue
+++ src/components/MusicContainer.vue
@@ -1,7 +1,11 @@
 <template>
-      <div>
-      {{ deezerData }}
+  <div>
+    <div class="info-music" v-for="(info, index) in deezerData" :key="index">
+      <p>{{ info.artist.name }}</p>
+      <p>{{ info.title }}</p>
+      <img :src="info.album.cover" >
     </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -11,23 +15,29 @@ export default {
     axios
       //http://api.deezer.com/playlist/436962 utilice un dominio diferente para no tener problemas con el CORS de api deezer. https://developers.deezer.com/api
       .get(
-        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/436962",
-        {
-          method: "GET",
-          crossDomain: true
-        }
+        "https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/436962/"
       )
       .then(res => {
-        this.deezerData = res.data;
-        console.log(res);
+        this.deezerData = res.data.tracks.data;
+        console.log(this.deezerData);
       });
   },
-  data(){
-      return{
-          deezerData :'',
-      }
+  data() {
+    return {
+      deezerData: "",
+    };
   }
 };
 </script>
 <style lang="scss">
+.container-info-music {
+  box-sizing: border-box;
+  padding-bottom: 82px;
+  margin-bottom: 0px;
+  margin: auto auto auto 89px;
+  .info-music {
+    background-color: green;
+    box-sizing: border-box;
+  }
+}
 </style>
